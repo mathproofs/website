98f264fd295d8fc750cd73599b1b07bcc849a23d
Jeroen van Rensen <jmjrensen@jcjcj.nl>
2022-08-17 18:31:40 +0200

    Design the navbar

--- resources/views/proofs/show.blade.php
+++ resources/views/proofs/show.blade.php
@@ -1,7 +1,9 @@
 <x-layout :title="$proof->title">
-    <h1 class="text-4xl font-bold">{{ $proof->title }}</h1>
+    <div class="max-w-screen-md mx-auto my-12">
+        <h1 class="text-4xl font-bold">{{ $proof->title }}</h1>
 
-    <div class="space-y-4">
-        {!! $proof->body !!}
+        <div class="space-y-4">
+            {!! $proof->body !!}
+        </div>
     </div>
 </x-layout>
