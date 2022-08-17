832b40925229735e7ebb5b55bac04a9917097e76
Jeroen van Rensen <jmjrensen@jcjcj.nl>
2022-08-17 17:50:12 +0200

    Install Pest PHP

---
+++ tests/Pest.php
@@ -0,0 +1,5 @@
+<?php
+
+uses(Tests\TestCase::class)->in('Feature', 'Unit');
+uses(Illuminate\Foundation\Testing\RefreshDatabase::class)->in('Feature', 'Unit');
+uses()->beforeEach(fn () => $this->withoutExceptionHandling())->in('Feature', 'Unit');
