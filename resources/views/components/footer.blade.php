b143bb0546c799e7991c99f0723b1ecb89c00b64
Jeroen van Rensen <jmjrensen@jcjcj.nl>
2022-08-18 17:44:48 +0200

    Update color: `gray` to `stone`

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="py-12 border-t border-gray-200">
+<footer class="py-12 border-t border-stone-200">
     <x-container>
         <p>&copy; {{ date('Y') }} by <a class="hover:underline" href="{{ route('home') }}">{{ config('app.name') }}</a></p>
     </x-container>
--- resources/views/proofs/index.blade.php
+++ resources/views/proofs/index.blade.php
@@ -7,7 +7,7 @@
         <div class="flex items-center w-full mb-12 space-x-4">
             @foreach ($categories as [$category, $categoryProofs])
                 <a
-                    class="px-4 py-3 font-medium text-center border rounded-md shadow-sm box {{ request('category') == $category->value ? 'border-yellow-500 ring-1 ring-yellow-500 font-bold' : 'border-gray-200' }}"
+                    class="px-4 py-3 font-medium text-center border rounded-md shadow-sm box {{ request('category') == $category->value ? 'border-yellow-500 ring-1 ring-yellow-500 font-bold' : 'border-stone-200' }}"
                     href="{{ route('proofs.index', ['category' => request('category') == $category->value ? null : $category->value, 'search' => request('search')]) }}"
                 >
                     {{ $category->name() }} <span class="text-stone-500">({{ $categoryProofs->count() }})</span>
