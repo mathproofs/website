ea8c67a0483c762d4dcb869779d2337677334c4b
Jeroen van Rensen <jmjrensen@jcjcj.nl>
2022-08-18 17:36:24 +0200

    Refactor proofs archive #2

--- app/Http/Controllers/ProofController.php
+++ app/Http/Controllers/ProofController.php
@@ -10,34 +10,27 @@ class ProofController extends Controller
 {
     public function index(): View
     {
+        // Find proofs
         if (request()->has('search')) {
-            $proofs = Proof::search(request('search'));
-
-            if (request()->has('category')) {
-                $proofs = $proofs->get()->filter(fn ($proof) => $proof->category->value == request('category'));
-            } else {
-                $proofs = $proofs->get();
-            }
+            $proofs = request()->has('category')
+                ? Proof::search(request('search'))->get()->filter(fn ($proof) => $proof->category->value == request('category'))
+                : Proof::search(request('search'))->get();
         } else {
-            if (request()->has('category')) {
-                $proofs = Proof::where('category', request('category'))->get();
-            } else {
-                $proofs = Proof::all();
-            }
+            $proofs = request()->has('category')
+                ? Proof::where('category', request('category'))->get()
+                : Proof::all();
         }
 
+        // Find categories
         $categories = collect(Category::cases())->map(function (Category $category) {
-            if (request()->has('search')) {
-                $proofs = Proof::search(request('search'))->get()->filter(fn ($proof) => $proof->category == $category);
-            } else {
-                $proofs = Proof::where('category', $category->value)->get();
-            }
+            $proofs = request()->has('search')
+                ? $proofs = Proof::search(request('search'))->get()->filter(fn ($proof) => $proof->category == $category)
+                : $proofs = Proof::where('category', $category->value)->get();
 
             return [$category, $proofs];
-        })->filter(function ($categoryArray) {
-            return $categoryArray[1]->count() > 0;
-        });
+        })->filter(fn ($categoryArray) => $categoryArray[1]->count() > 0);
 
+        // Return response
         return view('proofs.index', [
             'title' => request()->has('search') ? "Search results for '" . request('search') . "'" : 'Proof Archive',
             'proofs' => $proofs,
