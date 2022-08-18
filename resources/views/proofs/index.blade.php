159a3e8d455386663f5efed3efb764716f02d0c1
Jeroen van Rensen <jmjrensen@jcjcj.nl>
2022-08-18 17:08:14 +0200

    When clicking the active category, it should unlink

--- resources/views/proofs/index.blade.php
+++ resources/views/proofs/index.blade.php
@@ -15,7 +15,7 @@
                 @if($proofCount > 0)
                     <a
                         class="px-4 py-3 font-medium text-center border rounded-md shadow-sm box {{ request('category') == $category->value ? 'border-yellow-500 ring-1 ring-yellow-500 font-bold' : 'border-gray-200' }}"
-                        href="{{ route('proofs.index', ['category' => $category->value, 'search' => request('search')]) }}"
+                        href="{{ route('proofs.index', ['category' => request('category') == $category->value ? null : $category->value, 'search' => request('search')]) }}"
                     >
                         {{ $category->name() }} <span class="text-stone-500">({{ $proofCount }})</span>
                     </a>
