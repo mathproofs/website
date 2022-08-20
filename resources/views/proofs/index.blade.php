a9a4d6c7be5ab76817abdabe113e4ace435f590a
Jeroen van Rensen <jmjrensen@jcjcj.nl>
2022-08-20 13:24:10 +0200

    Fix design bug

--- resources/views/proofs/index.blade.php
+++ resources/views/proofs/index.blade.php
@@ -3,10 +3,10 @@
         <x-navbar />
     </header>
 
-    <main>
-        <x-container class="my-12 md:my-6">
-            <h1 class="mb-8 text-4xl font-medium md:mb-4">{{ $title }}</h1>
+    <x-container class="my-12 md:my-6">
+        <h1 class="mb-8 text-4xl font-medium md:mb-4">{{ $title }}</h1>
 
+        <aside>
             <ul class="flex items-center w-full py-px pl-px mb-12 space-x-4 overflow-x-auto md:mb-8">
                 @foreach ($categories as [$category, $categoryProofs])
                     <a
@@ -14,37 +14,39 @@
                         href="{{ route('proofs.index', ['category' => request('category') == $category->value ? null : $category->value, 'search' => request('search')]) }}"
                     >
                         <li>
-                                {{ $category->name() }} <span class="text-stone-500">({{ $categoryProofs->count() }})</span>
+                            {{ $category->name() }} <span class="text-stone-500">({{ $categoryProofs->count() }})</span>
                         </li>
                     </a>
                 @endforeach
-            </div>
+            </ul>
+        </aside>
 
-            @forelse ($proofs as $proof)
-                <article>
-                    <a href="{{ route('proofs.show', $proof) }}">
-                        <li class="mb-10">
-                            <header>
-                                @if($proof->category && !request()->has('category'))
-                                    <p class="mb-1 font-light text-yellow-600 md:mb-2">{{ $proof->category->name() }}</p>
-                                @endif
-                            </header>
+        <main>
+            <ul>
+                @forelse ($proofs as $proof)
+                    <article>
+                        <a href="{{ route('proofs.show', $proof) }}">
+                            <li class="mb-10">
+                                <header>
+                                    @if($proof->category && !request()->has('category'))
+                                        <p class="mb-1 font-light text-yellow-600 md:mb-2">{{ $proof->category->name() }}</p>
+                                    @endif
+                                </header>
 
-                            <main>
                                 <h3 class="mb-1 text-xl font-medium md:mb-2">{{ $proof->title }}</h3>
 
                                 @if($proof->description)
                                     <p class="text-stone-600">{{ $proof->description }}</p>
                                 @endif
-                            </main>
-                        </li>
-                    </a>
-                </article>
-            @empty
-                <li class="-mt-12">No results found.</li>
-            @endforelse
-        </x-container>
-    </main>
+                            </li>
+                        </a>
+                    </article>
+                @empty
+                    <li class="-mt-12">No results found.</li>
+                @endforelse
+            </ul>
+        </main>
+    </x-container>
 
     <x-footer />
 </x-layout>
