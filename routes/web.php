44ddb78d8974c068e2ca2e661b179885a7d4fbb0
Jeroen van Rensen <jmjrensen@jcjcj.nl>
2022-09-08 18:49:01 +0200

    Update route

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,6 @@ use App\Http\Controllers\ProofController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeController::class)->name('home');
-Route::get('/proofs', [ProofController::class, 'index'])->name('proofs.index');
+Route::get('/proofs/archive', [ProofController::class, 'index'])->name('proofs.index');
 Route::get('/proofs/{proof:slug}', [ProofController::class, 'show'])->name('proofs.show');
 Route::view('/social', 'social');
