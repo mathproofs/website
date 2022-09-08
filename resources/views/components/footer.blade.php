cbe870d372a62d695e0e7297120673cb39b9f7de
Jeroen van Rensen <jmjrensen@jcjcj.nl>
2022-09-08 11:41:17 +0200

    Update footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="py-12 border-t md:py-6 border-stone-200">
     <x-container>
-        <p>&copy; {{ date('Y') }} by <a class="hover:underline" href="{{ route('home') }}">{{ config('app.name') }}</a></p>
+        <p>&copy; {{ date('Y') }} by <a class="hover:underline" href="https://jeroenvanrensen.nl/">Jeroen van Rensen</a></p>
     </x-container>
 </footer>
